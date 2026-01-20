22ffe2685cf81d093b9a27674e1e87699f9379bc
gabrosa <gabriel.melo@bandtec.com.br>
2026-01-19 22:51:46 -0300

    Inclusão dos novos formularios

--- frontend/src/pages/Conversor.jsx
+++ frontend/src/pages/Conversor.jsx
@@ -1,5 +1,15 @@
-import { Box, Text, Container} from '@chakra-ui/react';
+import { Box, Text, Container, Heading, Stack, HStack, Field} from '@chakra-ui/react';
 import '../css/conversor.css'
+import { Button } from '@chakra-ui/react';
+import { NumberInput} from "@chakra-ui/react"
+
+function converterParaReais(){
+    console.log("Vou converter para reais");
+}
+
+function converterDinheiroBruxo(){
+    console.log("Vou converter vou converter para dinheiro bruxo");
+}
 
 function Conversor(){
     return <>
@@ -9,11 +19,60 @@ function Conversor(){
             </Box>
         </Container>
         
-        <div className="texto">
-            No mundo bruxo tudo é diferente, incluseive o próprio dinheiro. <br/>
-            Pra você que quer saber quanto dinheiro trouxa precisa ou ao contrário <br/>
-            Basta verificar abaixo:
-        </div>
+        <Container className="texto">
+            <Box >
+                <Text>
+                    No mundo bruxo tudo é diferente, incluseive o próprio dinheiro. 
+                    Pra você que quer saber quanto dinheiro trouxa precisa ou ao contrário 
+                    Basta verificar abaixo:
+                </Text>
+            </Box>
+        </Container>
+
+        <Container>
+            <HStack>
+                <Box backgroundColor="black">
+                <Heading size="4xl">Bruxo para Trouxa</Heading>
+                <Stack>
+                    {/* Input das moedas de ouro */}
+                    <Field.Root>
+                        <Field.Label>
+                            Galeão
+                        </Field.Label>
+                            <NumberInput.Root defaultValue="10" width="200px">
+                            {/* <NumberInput.Control /> */}
+                            <NumberInput.Input />
+                        </NumberInput.Root>
+                    </Field.Root>
+
+                    {/* Input das moedas de prata */}
+                    <Field.Root>
+                        <Field.Label>
+                            Sicles
+                        </Field.Label>
+                            <NumberInput.Root defaultValue="10" width="200px">
+                            {/* <NumberInput.Control /> */}
+                            <NumberInput.Input />
+                        </NumberInput.Root>
+                    </Field.Root>
+                    
+                </Stack>
+                <Button variant="ghost" onClick={converterParaReais} >Converter</Button>
+            </Box>
+                <Box backgroundColor="black">
+                    <Heading size="4xl">Trouxa para Bruxo</Heading>
+                    <Stack>
+                        <NumberInput.Root defaultValue="10" width="200px">
+                            {/* <NumberInput.Control /> */}
+                            <NumberInput.Input />
+                        </NumberInput.Root>
+                    </Stack>
+                    <Button variant="ghost" onClick={converterParaReais} >Converter</Button>
+                </Box>
+            </HStack>
+            
+            
+        </Container>
 
         <div className="conversores">
             <div className="box">
@@ -22,7 +81,7 @@ function Conversor(){
                     Galeão: <input id="valor_galeao" type="text"/> <br/>
                     Sicles : <input id="valor_sicle" type="text"/> <br/>
                     Nuquel: <input id="valor_nuquel" type="text"/> <br/>
-                    <button onclick="converter_1()" >Converter</button>
+                    <Button variant="ghost" onClick={converterParaReais} >Converter</Button>
                 </p>
                 <p id="valor_real">
                 </p>
@@ -32,7 +91,7 @@ function Conversor(){
                 <p>
                     Quantos reais você possui: <br/>
                     <input id="valor_reais" placeholder="R$" type="text"/> <br/>
-                    <button onclick="converter_2()">Converter</button>
+                    <button onClick={converterDinheiroBruxo}>Converter</button>
                 </p>
                 <p id="valor_bruxo">
                 </p>
